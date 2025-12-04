716183f937f49e09fc4db82ff79a44d6d1bfa53e
henryh0dgs0n2007-wq <henryh0dgs0n2007@gmail.com>
2025-12-04 16:24:05 +0000

    Bomboclaaat

--- LaunchClimb.m
+++ LaunchClimb.m
@@ -22,12 +22,12 @@ CL = CL0+(lGrad/(1+(lGrad/(pi*aspectRatio*oswaldEff))))*InitialChordAngle;
 Lift = 0.5*AirDensity*launchSpeed^2*wingArea*CL;
 CD = CD0 + CoeffI*CL^2; 
 Drag = -1*0.5*AirDensity*launchSpeed^2*wingArea*CD;
-Weight = -1*Mass*9.81;
+Weight = Mass*9.81;
 StartAngle = 0;
 ClimbAngle = asind(Tension/Weight-CD/CL);
 %% resolve forces
 Fx = Tension+Drag;      % horizontal force (N)
-Fy = Lift+Weight;      % vertical force (N)
+Fy = Lift-Weight;      % vertical force (N)
 %% make graph
 figure; hold on; grid on; axis equal;
 xlim([0 10]); ylim([0 10]);
@@ -50,11 +50,11 @@ plot(displacementX, displacementY, 'o', 'MarkerSize', 6, 'MarkerFaceColor', 'b')
 for k = 1:15
     % update forces based new position
     time = time + 0.1
-WinchLength = StartingWinchLength-launchSpeed*time
+WinchLength = StartingWinchLength-launchSpeed*time;
 Tension = (Mass*launchSpeed^2)/WinchLength;
 winchangle = asind(displacementX/WinchLength);
 TensionX = Tension*(sind(winchangle+InitialChordAngle));
-TensionY = -1*Tension*(cosd(winchangle+InitialChordAngle))
+TensionY = Tension*(cosd(winchangle+InitialChordAngle));
 % update L/D based on new climb angle
 CL = CL0+(lGrad/(1+(lGrad/(pi*aspectRatio*oswaldEff))))*(InitialChordAngle+ClimbAngle);
 CD = CD0 + CoeffI*CL^2;
@@ -64,8 +64,8 @@ Drag = -1*0.5*AirDensity*VelocityX^2*wingArea*CD;
 Lift = 0.5*AirDensity*VelocityX^2*wingArea*CL;
 LiftX = -1*Lift*sind(ClimbAngle);
 LiftY = Lift*cosd(ClimbAngle);
-Fx = TensionX+Drag+LiftX;      % horizontal force (N)
-Fy = LiftY+Weight+TensionY;      % vertical force (N)
+Fx = TensionX+Drag+LiftX      % horizontal force (N)
+Fy = LiftY-Weight-TensionY      % vertical force (N)
 %% Calculate acceleration
 accelerationX = Fx / Mass;  % horizontal acceleration (m/s^2)
 accelerationY = Fy / Mass; % vertical acceleration (m/s^2)
