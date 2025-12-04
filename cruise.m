1ff298d1ccf9d669841628ca861827a599f731a5
egyht4 <egyht4@nottingham.ac.uk>
2025-12-04 11:59:48 +0000

    updated mass

--- cruise.m
+++ cruise.m
@@ -1,5 +1,5 @@
 %% Variables
-Mass = 0.135;
+Mass = 0.092;
 wingArea = 0.1066667;
 aspectRatio = 6;
 oswaldEff = 0.95;
@@ -16,10 +16,11 @@ CL0     = 0.29;       %
 a_deg   = 0.085;      % gradient of cl vs alpha (degrees)
 a_rad   = a_deg * (180/pi);   % alpha in radians
 
-CD0_wing = 0.023;     % wing profile drag at low CL
+CD0_wing = 0.023;     % wing profile drag cl0
 CD0      = 2 * CD0_wing;  % whole-glider zero-lift drag
 
-k = 1 / (pi * oswaldEff * aspectRatio);  % induced drag factor
+k = 1 / (pi * oswaldEff * aspectRatio);  % induced drag factor (k)
 
 %% Glide modelling
-a_glide_deg = 2.5
+a_glide_deg = 2.5;
+a_glide_rad = a_glide_deg * (180/pi);
