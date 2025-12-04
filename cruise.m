1b1e2ab8084f2b8ee3de338ffeb66dc51d46cd10
egyht4 <egyht4@nottingham.ac.uk>
2025-12-04 11:31:13 +0000

    Added glide paramaters and modelling section

--- cruise.m
+++ cruise.m
@@ -1,12 +1,25 @@
 %% Variables
-Mass = 135
-wingArea = 0.1066667
-aspectRatio = 6
-oswaldEff = 0.95
-launchSpeed = 5
-launchHeight = 0.2
-launchTime = 1.5
-launchDist = 3
+Mass = 0.135;
+wingArea = 0.1066667;
+aspectRatio = 6;
+oswaldEff = 0.95;
+rho = 1.225;
+g = 9.81;
 
+% variables from climb
+releaseHeight = 8;          
+releaseSpeed  = 5;          
+releaseDist   = 7.5;
 
-%% Cruise
+%% Glide parameters
+CL0     = 0.29;       % 
+a_deg   = 0.085;      % gradient of cl vs alpha (degrees)
+a_rad   = a_deg * (180/pi);   % alpha in radians
+
+CD0_wing = 0.023;     % wing profile drag at low CL
+CD0      = 2 * CD0_wing;  % whole-glider zero-lift drag
+
+k = 1 / (pi * oswaldEff * aspectRatio);  % induced drag factor
+
+%% Glide modelling
+a_glide_deg = 2.5
