b53d982a2ec1bc58c7587b55011a5aa4602b1dbe
egyht4 <egyht4@nottingham.ac.uk>
2025-12-04 14:22:29 +0000

    finished calculations and added desc to top

--- cruise.m
+++ cruise.m
@@ -1,18 +1,26 @@
+% Harry Toms - egyht4 
+% This script takes the variables calculated from our spreadsheet and
+% predicts a glide path, range and AOA. It predicts a "best case", by
+% iterating through the AOA between 1 and 8 degrees, and solves for the
+% best AOA and the range that will be achieved. It also then solves for a
+% more realistic AOA, which we assumed to be 5 degrees. 
 %% Variables
-Mass = 0.092;
+Mass = 0.11;
 wingArea = 0.1066667;
 aspectRatio = 6;
 oswaldEff = 0.95;
 rho = 1.225;
 g = 9.81;
 
+
 % variables from climb
-releaseHeight = 8;          
+releaseHeight = 2.5;          
 releaseSpeed  = 5;          
 releaseDist   = 7.5;
 
 %% Glide parameters
-CL_0     = 0.29;       % 
+CL_0     = 0.29;       
+CL_max = 0.927;      % from JavaFoil 
 a_deg   = 0.085;      % gradient of cl vs alpha (degrees)
 a_rad   = a_deg * (180/pi);   % alpha in radians
 
@@ -45,12 +53,44 @@ for i = 1:n
     end
 end
 
-fprintf('Best AoA           = %.2f deg\n', alpha_best);
-fprintf('Max total range    = %.2f m\n', totalRange_best);
+fprintf('Best AoA           = %.2f deg\n', bestAlpha);
+fprintf('Max total range    = %.2f m\n', bestRange);
 
 % Glide performance at best AOA
-CL_glide = CL_0 + a_deg * alphaBest;
-CD_glide = CD_0 + k * (CL_glide).^2;
+CL_glide = CL_0 + a_deg * bestAlpha;
+CD_glide = CD0 + k * (CL_glide).^2;
 LD_glide = CL_glide / CD_glide;
 
 glideRange  = LD_glide * releaseHeight;
+
+
+%% Glide performance for expected AOA
+alpha_design_deg = 5;
+CL_design = CL_0 + a_deg * alpha_design_deg;
+CD_design = CD0 + k * (CL_design).^2;
+LD_design = CL_design / CD_design;
+
+glideRange_design = LD_design * releaseHeight;
+totalRange_design = releaseDist + glideRange_design;
+
+% Speed for this CL
+W = Mass * g;
+V_design = sqrt((2 * W) / (rho * wingArea * CL_design));
+glideTime_design = glideRange_design / V_design;
+
+% Calculate v min drag and v stall
+V_stall = sqrt( (2 * W) / (rho * wingArea * CL_max) );
+CL_VMD = sqrt(CD0 / k);
+V_min_drag = sqrt( (2 * W) / (rho * wingArea * CL_VMD) );
+
+fprintf('\nActual design AoA case:\n');
+fprintf('  alpha_design       = %.2f deg\n', alpha_design_deg);
+fprintf('  CL_design          = %.3f\n', CL_design);
+fprintf('  CD_design          = %.3f\n', CD_design);
+fprintf('  L/D_design         = %.2f\n', LD_design);
+fprintf('  V_stall      = %.2f m/s\n', V_stall);
+fprintf('  V_min_drag   = %.2f m/s (at CL_VMD = %.3f)\n', V_min_drag, CL_VMD);
+fprintf('  V_glide (design)   = %.2f m/s\n', V_design);
+fprintf('  Glide range        = %.2f m\n', glideRange_design);
+fprintf('  Total range        = %.2f m\n', totalRange_design);
+fprintf('  Glide time         = %.2f s\n', glideTime_design);
